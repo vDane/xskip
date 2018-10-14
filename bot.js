7e19e191c14408f15a7bbb5e35c328e404f9934b
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-14 10:13:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -844,6 +844,16 @@ client.on('message', message => {
     }
 })
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Chyper +help`,"http://twitch.tv/Cypher Bot")
+client.user.setStatus("dnd")
+});
 
 
 
