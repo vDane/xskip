2c16904f0daae9547226ff0562226b2aaedd070c
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-18 11:43:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,51 +1,48 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "^";
-client.on('message', message => { // Leaked by [ @M3a4x ]
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return
-const args = message.content.split(" ").slice(1).join(" ")
-const BcList = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.setAuthor(`محتوى الرساله : ${args}`)
-.setDescription(`**برودكاست بـ امبد ??\nبرودكاست بدون امبد? \nلديك دقيقه للأختيار قبل الغاء البرودكاست**`)
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
-msg.react('??')
-.then(() => msg.react('?'))
-.then(() =>msg.react('??'))
- 
-var EmbedBcFilter = (reaction, user) => reaction.emoji.name === '??' && user.id === message.author.id;
-var NormalBcFilter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
- 
-var EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
-var NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
- 
- 
-EmbedBc.on("collect", r => {
- 
-message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-var EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setDescription(EmbedRep)
-.setThumbnail(message.author.avatarURL)
-m.send({ embed: bc })
-msg.delete();
-})
-})
-NormalBc.on("collect", r => {
-  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-var NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-m.send(NormalRep);
-msg.delete();
-})
-})
-})
+client.on('message', message => {
+    var prefix = '^'; // هنا تقدر تغير البرفكس
+var command = message.content.split(" ")[0];
+if(command == prefix + 'bc') { // الكوماند !bc
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(message.author.bot) return;
+    if(!args) return message.channel.send(`**➥ Useage:** ${prefix}bc كلامك`);
+   
+    let bcSure = new Discord.RichEmbed()
+    .setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
+    .setThumbnail(client.user.avatarURL)
+    .setColor('RANDOM')
+    .setDescription(`**\n:envelope: ➥ رسالتك**\n\n${args}`)
+    .setTimestamp()
+    .setFooter(message.author.tag, message.author.avatarURL)
+   
+    message.channel.send(bcSure).then(msg => {
+        msg.react('✅').then(() => msg.react('❎'));
+        message.delete();
+       
+       
+        let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+        let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+       
+        let sendBC = msg.createReactionCollector(yesEmoji);
+        let dontSendBC = msg.createReactionCollector(noEmoji);
+       
+        sendBC.on('collect', r => {
+            message.guild.members.forEach(member => {
+                member.send(args.replace(`[user]`, member)).catch();
+                if(message.attachments.first()){
+                    member.sendFile(message.attachments.first().url).catch();
+                }
+            })
+            message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
+            msg.delete();
+        })
+        dontSendBC.on('collect', r => {
+            msg.delete();
+            message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
+        });
+    })
 }
 });
 
