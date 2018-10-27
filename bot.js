c4922d938e081fbbc0bfc7c7fa709314312f926a
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-27 11:04:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,53 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "^";
-     client.on('message' , message => {
-          var prefix = "^";
-          if(message.author.bot) return;
-         
-          if(message.content.startsWith(prefix + "bcc")) {
-            if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-            let args = message.content.split(" ").slice(1);
-         
-            if(!args[0]) {
-              message.channel.send("قم بمنشنة الرتبة | !rolebc @everyone message")
-                return;
-            }
-            if(!args[1]) {
-              message.channel.send("قم بكتابة الرسالة | !rolebc @everyone message")
-                return;
-            }
-         
-              if(args[0] == "@everyone") {
-                message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.memberCount} اعضاء`)
-                message.guild.members.forEach(m => {
-                  m.send(
-                  "**" + "السيرفر :" + "\n" +
-                  `${message.guild.name}` + "\n" +
-                  "المرسل :" + "\n" +
-                  `${message.author.tag}` + "\n" +
-                  "الرسالة :" + "\n" +
-                  `${args[1]}` + "**"
-                  )
-                })
-                return;
-              }
-         
-                  var role = message.mentions.roles.first();
-                    if(!role) {
-                      message.reply("لا توجد رتبة بهذا الاسم")
-                        return;
-                    }
-                message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
-                  n.send(
-                  "**" + "السيرفر :" + "\n" +
-                  `${message.guild.name}` + "\n" +
-                  "المرسل :" + "\n" +
-                  `${message.author.tag}` + "\n" +
-                  "الرسالة :" + "\n" +
-                  `${args[1]}` + "**"
-                  )
-                })
-                message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
-            }
-        });
+client.on('message', message => { // Leaked by [ @M3a4x ]
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return
+const args = message.content.split(" ").slice(1).join(" ")
+const BcList = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.setAuthor(`محتوى الرساله : ${args}`)
+.setDescription(`**برودكاست بـ امبد ??\nبرودكاست بدون امبد? \nلديك دقيقه للأختيار قبل الغاء البرودكاست**`)
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+msg.react('??')
+.then(() => msg.react('?'))
+.then(() =>msg.react('??'))
+ 
+var EmbedBcFilter = (reaction, user) => reaction.emoji.name === '??' && user.id === message.author.id;
+var NormalBcFilter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
+ 
+var EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+var NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+ 
+ 
+EmbedBc.on("collect", r => {
+ 
+message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription(EmbedRep)
+.setThumbnail(message.author.avatarURL)
+m.send({ embed: bc })
+msg.delete();
+})
+})
+NormalBc.on("collect", r => {
+  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+m.send(NormalRep);
+msg.delete();
+})
+})
+})
+}
+});
 
   client.on('message', message => {
     if (message.content.startsWith("^Link")) {
