3de7b5bfcb242d26cb852ec7ce138ea5972c6dc6
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-30 11:43:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1420,4 +1420,35 @@ client.on('message', function(message) {
     }
 });
 
+client.on('message', message => {
+    if(message.content == '^serversss') {
+             if(!message.author.id === '472413769700474901') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    gbots = g.members.filter(m=>m.bot).size;
+    groles = g.roles.map(r=> {return r.name});
+    let serv = new Discord.RichEmbed()
+    .setAuthor(gname,gimg)
+    .setThumbnail(gimg)
+    .addField('Server bots',gbots)
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+    Server Name : **${gname}**
+    Server MemberCount : **${gmemb} **
+            
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
