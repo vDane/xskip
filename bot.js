d0776979310f54c22df44846ed4145c21781436a
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-30 11:16:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1365,5 +1365,28 @@ client.on('message', message => {
   }  
  });
  
+client.on('message', message => {
+    if (message.content.includes('disc ord.gg')){
+                        if(!message.channel.guild) return message.reply ('')
+                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
+       message.channel.send('kick <@' + message.author.id + '>')
+       message.delete() 
+       }
+    }
+          if (message.content.startsWith("kick ")) {
+             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
+             var member= message.mentions.members.first();
+             member.ban().then((member) => {
+                 message.channel.sendMessage("", {embed: {
+                 author: {
+                 },
+                 title: 'بسبب النشر ' + member.displayName + ' تم طرد',
+                 color: 490101,
+                 }
+               });
+           }
+         ) 
+       }
+   });
  
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
