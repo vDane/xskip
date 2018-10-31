6fd9bc2720dc345fb8e019b2c3dcbe386df47671
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-31 16:38:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -862,7 +862,7 @@ client.on('message', message => {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
+  if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/zero");
       message.channel.send(`**✅**`)
   }
